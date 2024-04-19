28f2209d1e990bc731dc86cb0686bce8e9418b1f
ndagui <ndaguijeanlouismelanie@gmail.com>
2024-04-19 18:12:16 +0100

    adding front-end

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :t
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
 let persons = [
     {
         "id": 1,
