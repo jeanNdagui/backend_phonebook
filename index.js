1d4c11ca87a6bd959edde5359dd8a4acf9cbf866
ndagui <ndaguijeanlouismelanie@gmail.com>
2024-04-24 18:40:03 +0100

    add eslint

--- index.js
+++ index.js
@@ -7,101 +7,94 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({ error: 'content missing' })
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  Person.find({}).then(person => {
+    if (person.find(p => p.name.toLowerCase() === body.name.toLowerCase()).length !== 0) {
+      return response.status(403).json({ error: `${body.name} already exist in phonebook.` })
+    } else {
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
+      }).catch(error => response.status(400).json({ error: error.message }))
     }
-
-    Person.find({}).then(person => {
-        if (person.find(p => p.name.toLowerCase() === body.name.toLowerCase()).length !== 0) {
-            return response.status(403).json({ error: `${body.name} already exist in phonebook.` })
-        } else {
-            const person = new Person({
-                name: body.name,
-                number: body.number,
-            })
-
-            person.save().then(savedPerson => {
-                response.json(savedPerson)
-            }).catch(error => response.status(400).json({ error: error.message }))
-        }
-
-
-    })
-
-
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => response.status(204).end())
-        .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(person => {
-        response.send(
+  Person.find({}).then(person => {
+    response.send(
             `<div>
                 Phonebook has info for ${person.length} people<br>
                  ${new Date()}
             </div>`
-        )
-    })
-
+    )
+  })
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
-
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
+  next(error)
 }
 
 // this has to be the last loaded middleware.
@@ -109,5 +102,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
