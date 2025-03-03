d5e8e15ff5b627414d373a159026334ad968a1a9
jeanNdagui <ndaguijeanlouismelanie@gmail.com>
2025-03-03 02:49:23 +0100

    exercise part3

---
+++ index.js
@@ -0,0 +1,104 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const Person = require("./model/Person");
+
+const app = express();
+
+app.use(express.static("dist"));
+
+app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms - :data"
+  )
+);
+morgan.token("data", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => response.json(persons));
+});
+
+app.get("/info", (request, response) => {
+  Person.find({}).then((persons) =>
+    response.send(`<div>Phonebook has info for ${persons.length} people.</div> 
+    <div>${Date()}</div>`)
+  );
+});
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.json(result);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body;
+  console.log(body);
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((personSaved) => response.json(personSaved))
+    .catch((err) => next(err));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true ,   runValidators: true, context: 'query' })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
